74674ea5cab96cbbfac5773ab60d68c8d5c7c0b6
Aleksandra Baibik <abaibik@gmail.com>
2023-01-21 23:34:35 +0100

    Mock service worker setup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
+import SummaryForm from "./pages/summary/SummaryForm";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <SummaryForm />
+    </div>
+  );
 }
 
 export default App;
