c970ee63d775b6fe1268004de580ba1e3cc75ac1
Aleksandra Baibik <abaibik@gmail.com>
2023-02-05 10:51:38 +0100

    OrderSummary shows lists of scoops and toppings

---
+++ src/pages/summary/OrderSummary.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import SummaryForm from "./SummaryForm";
+import { useOrderDetails } from "../../contexts/OrderDetails";
+import { formatCurrency } from "../../utilities";
+
+export default function OrderSummary() {
+  const { totals, optionCounts } = useOrderDetails();
+
+  const scoopArray = Object.entries(optionCounts.scoops);
+  const scoopsList = scoopArray.map(([key, value]) => (
+    <li key={key}>
+      {value} {key}
+    </li>
+  ));
+
+  const toppingsArray = Object.keys(optionCounts.toppings);
+  const toppingsList = toppingsArray.map((name) => <li key={name}>{name}</li>);
+
+  return (
+    <div>
+      <h1>OrderSummary</h1>
+      <h2>Scoops: {formatCurrency(totals.scoops)}</h2>
+      <ul>{scoopsList}</ul>
+      <ul>{toppingsList}</ul>
+      <h2>Toppings: {formatCurrency(totals.toppings)}</h2>
+      <SummaryForm />
+    </div>
+  );
+}
