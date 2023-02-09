196576e6879f24284e1f71b3d2ad291c8072a52e
Aleksandra Baibik <abaibik@gmail.com>
2023-02-09 13:24:17 +0100

    update toppings subtotal when toppings change

--- src/pages/entry/tests/totalUpdates.test.jsx
+++ src/pages/entry/tests/totalUpdates.test.jsx
@@ -28,3 +28,34 @@ test("update scoop subtotal when scoops change", async () => {
 
   expect(scoopsSubtotal).toHaveTextContent("6.00");
 });
+
+test("update toppings subtotal when toppings change", async () => {
+  const user = userEvent.setup();
+  render(<Options optionType="toppings" />);
+
+  const toppingsSubtotal = screen.getByText("Toppings total: €", {
+    exact: false,
+  });
+
+  expect(toppingsSubtotal).toHaveTextContent("0.00");
+
+  const cherriesCheckbox = await screen.findByRole("checkbox", {
+    name: "Cherries",
+  });
+
+  await user.click(cherriesCheckbox);
+
+  expect(toppingsSubtotal).toHaveTextContent("1.50");
+
+  const hotHudgeCheckbox = await screen.findByRole("checkbox", {
+    name: "Hot hudge",
+  });
+
+  await user.click(hotHudgeCheckbox);
+
+  expect(toppingsSubtotal).toHaveTextContent("3.00");
+
+  await user.click(hotHudgeCheckbox);
+
+  expect(toppingsSubtotal).toHaveTextContent("1.50");
+});
